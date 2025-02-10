d38b484721d5be05f51f5774f01d9435dead9d3e
fgrreloaded <nk.nitish.nk500@gmail.com>
2025-02-10 22:54:42 +0530

    added tedxglau website

--- projects.js
+++ projects.js
@@ -5,7 +5,7 @@ const projects = [
     description: "Interactive website with GSAP animations",
     thumbnail: "./projects/Alice-Ai/thumbnail.png",
     path: "./projects/Alice-Ai/index.html",
-    technologies: ["HTML", "CSS", "JS", "GSAP"]
+    technologies: ["HTML", "CSS", "JS", "GSAP"],
   },
   {
     id: "2",
@@ -13,7 +13,7 @@ const projects = [
     description: "Interactive website with GSAP animations",
     thumbnail: "./projects/anchor-web/thumbnail.png",
     path: "./projects/anchor-web/index.html",
-    technologies: ["HTML", "CSS", "JS", "GSAP"]
+    technologies: ["HTML", "CSS", "JS", "GSAP"],
   },
   {
     id: "3",
@@ -21,6 +21,14 @@ const projects = [
     description: "Gallery with draggable images",
     thumbnail: "./projects/draggable-gallery/thumbnail.png",
     path: "./projects/draggable-gallery/index.html",
-    technologies: ["HTML", "CSS", "JS"]
-  }
+    technologies: ["HTML", "CSS", "JS"],
+  },
+  {
+    id: "4",
+    title: "TEDxGLAU",
+    description: "Website for TEDx event",
+    thumbnail: "./projects/tedxglau/thumbnail.png",
+    path: "tedxglau.com",
+    technologies: ["Next.js", "Tailwind CSS", "Frame Motion", "Aceternity UI"],
+  },
 ];
